efed283f11d3cc5d915ee0c6bf4892d70da9d7e3
Arex <arexchu9@gmail.com>
2025-01-22 16:13:39 +0800

    use system time

--- bidHelper.py
+++ bidHelper.py
@@ -6,8 +6,7 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 import time
 from datetime import datetime, timedelta
 from pynput import keyboard
-import threading
-#import os
+from multiprocessing import Process
 import platform
 
 class LoginPage:
@@ -20,9 +19,11 @@ class LoginPage:
             if element is not None:
                 return element
         except StaleElementReferenceException:
-            print(f"find_element Failed to interact with element {by} = {value}")
+            pass
+            #print(f"find_element Failed to interact with element {by} = {value}")
         except NoSuchElementException:
-            print(f"find_element Failed to find element {by} = {value}")
+            pass
+            #print(f"find_element Failed to find element {by} = {value}")
         return None
 
     def element_text(self, by, value):
@@ -60,7 +61,7 @@ class LoginPage:
                 print(f"Attempt {i+1} of element_click failed to interact with element {by} = {value}. Retrying...")
             except NoSuchElementException:
                 print(f"Attempt {i+1} of element_click Failed to find element {by} = {value}. Retrying...")
-        print(f"element_click: Failed to find and interact with element {by} = {value} after {retries} attempts")
+        #print(f"element_click: Failed to find and interact with element {by} = {value} after {retries} attempts")
         return None
 
     @property
@@ -77,6 +78,13 @@ class LoginPage:
             current_time_str = element
             return datetime.strptime(current_time_str, "%H:%M:%S").time()
         return None
+    @property
+    def confirm_time(self):
+        element = self.element_text(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[3]/div[2]/div[1]/div[2]/span[3]')
+        if element is not None:
+            confirm_time_str = element.split("：")[-1]
+            return datetime.strptime(confirm_time_str, "%Y-%m-%d %H:%M:%S").time()
+        return None
     def set_add_price(self, add_price):
         element = self.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[3]/div[2]/div[2]/div/div[2]/div[1]/div[2]/div/input')
         if element is not None:
@@ -89,6 +97,11 @@ class LoginPage:
 
     def bid(self):
         element = self.element_click(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[3]/div[2]/div[2]/div/div[2]/div[3]/div[3]/div/span')
+        if element is not None:
+            element.click()
+
+    def first_bid(self):
+        element = self.element_click(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[3]/div[2]/div[2]/div/div[2]/div[2]/div[2]/span')
         if element is not None:
             element.click()
 
@@ -130,8 +143,6 @@ class LoginPage:
             return element
         return None
 
-# init env
-#url = os.environ['URL']
 architecture = platform.machine()
 if architecture == 'x86_64':
     service = Service('/usr/local/bin/chromedriver')
@@ -148,7 +159,6 @@ options.add_experimental_option("debuggerAddress", "127.0.0.1:8000")
 driver = webdriver.Chrome(service=service, options=options)
 
 # Open the webpage
-#driver.get(url)
 driver.switch_to.window(driver.window_handles[-1])
 
 # Instantiate LoginPage
@@ -181,50 +191,107 @@ def bid_help(add_price):
     login_page.bid()
     login_page.edit_verification_code()
 
+def first_bid_help():
+    login_page.first_bid()
+    login_page.edit_verification_code()
+
 def price_listener():
     print("price listener stop")
     while True:
-        time.sleep(0.1)
         max_price = login_page.max_price
         bid_price = login_page.bid_price
-        print(f'max price: {max_price}')
+        #print(f'max price: {max_price}')
         if max_price is not None and bid_price is not None:
-            print(f'bid price: {bid_price}')
+            #print(f'bid price: {bid_price}')
             verification_code = login_page.verification_code
             price_difference = int(max_price) - int(bid_price)
             if 0 <= price_difference <= 300  and len(verification_code) == 4:
                 print(f'verification code: {verification_code}')
                 print(f'price listener stop {bid_price}')
-                time.sleep(0.1)
                 login_page.confirm_verification_code()
+                confirm_time = datetime.now()
+                formatted_time = confirm_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                print(f'bid price {bid_price}, max price {max_price}, verification code {verification_code}, price confirm {formatted_time}')
             else:
-                print(f'bid price {bid_price} is too bigger than max price {max_price}, or {verification_code} not right')
+                print(f'bid price {bid_price} is too bigger than max price {max_price}, or verification code {verification_code} not right')
 
 def time_listener(seconds):
     print("time listener start")
     while True:
-        time.sleep(0.1)
         current_time = login_page.current_time
         if current_time is not None:
             time_difference = datetime.combine(datetime.today(), deadline_time) - datetime.combine(datetime.today(), current_time)
             print(f'time difference: {time_difference}')
             if time_difference <= timedelta(seconds=seconds):
                 print(f'time listener stop {current_time}')
-                time.sleep(0.1)
                 login_page.confirm_verification_code()
-                confirm_time = datetime.now()
-                formatted_time = confirm_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-                print(f'confirm time: {formatted_time}')
+                #confirm_time = datetime.now()
+                #formatted_time = confirm_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                #print(f'time confirm: {formatted_time}')
+                print(f'confirm time: {current_time}')
             else:
                 print(f'current time: {current_time}')
 
+def sys_time_listener(sys_seconds):
+    print("sys time listener start")
+    while True:
+        #current_time = login_page.current_time
+        now = datetime.now()
+        seconds = now.second
+        microseconds = now.microsecond
+        fractional_seconds = microseconds / 1_000_000
+        total_seconds = seconds + fractional_seconds
+        if abs(total_seconds - sys_seconds) < 0.05:
+            login_page.confirm_verification_code()
+            #print(f'time confirm: {current_time}')
+            print(f'sys confirm time: {now}')
+            time.sleep(1)
+            confirm_time = login_page.confirm_time
+            print(f'remote confirm time: {confirm_time}')
+            time.sleep(1)
+            login_page.confirm_bid()
+        else:
+            pass
+            #print(f'current time: {now}')
 
-# Create and start a new thread for the time listener
-price_thread = threading.Thread(target=price_listener)
-price_thread.start()
+def bid_time_listener(price, sys_seconds, bid):
+    print("bid time listener start")
+    while True:
+        now = datetime.now()
+        seconds = now.second
+        microseconds = now.microsecond
+        fractional_seconds = microseconds / 1_000_000
+        total_seconds = seconds + fractional_seconds
+        if abs(total_seconds - sys_seconds) < 0.05:
+            if bid:
+                bid_help(price)
+            else:
+                first_bid_help()
+            #print(f'bid confirm time: {now}')
+        else:
+            pass
+            #print(f'current time: {now}')
 
-time_thread = threading.Thread(target=time_listener, args=(1,))
-time_thread.start()
+def main():
+    production = False
+    bid = True
+    if production:
+        #time_process = Process(target=time_listener, args=(1,))
+        #time_process.start()
+        sys_time_process = Process(target=sys_time_listener, args=(59.50,))
+        sys_time_process.start()
+        bid_price_process = Process(target=bid_time_listener, args=(700,50,bid,))
+        bid_price_process.start()
+        price_process = Process(target=price_listener)
+        price_process.start()
+        key_process = Process(target=key_listener)
+        key_process.start()
+    else:
+        delta = 0.01
+        for i in range(60, 0, -10):
+            exec(f"bid_price_process_{i} = Process(target=bid_time_listener, args=(9000,{i-7},bid,))\nbid_price_process_{i}.start()")
+            exec(f"sys_time_process_{i} = Process(target=sys_time_listener, args=({i-delta},))\nsys_time_process_{i}.start()")
+            delta += 0.01
 
-key_thread = threading.Thread(target=key_listener)
-key_thread.start()
+if __name__ == '__main__':
+    main()
